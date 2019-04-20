0b4d0a1106c7d1f15bbe3b4e0950a4df970ba1d5
zhuanghaixin <649308293@qq.com>
2019-04-20 13:55:10 +0800

    [vue造轮子]:使用chai.spy监听回调函数

--- src/app.js
+++ src/app.js
@@ -18,78 +18,97 @@ new Vue({
 
 import chai from 'chai'
 const expect=chai.expect
+import spies from 'chai-spies'
+chai.use(spies)
 //单元测试
-//gButton setting
+//vm setting
 {
     const Constructor = Vue.extend(Button)
-    const gButton=new Constructor({
+    const vm=new Constructor({
         propsData: {
             icon:'setting'
         }
     })
 
-    gButton.$mount()
-    let useElement=gButton.$el.querySelector('use')
+    vm.$mount()
+    let useElement=vm.$el.querySelector('use')
     let href=useElement.getAttribute('xlink:href')
     expect(href).to.eq('#icon-setting')
     //清除button,防止内存占用
-    gButton.$el.remove()
-    gButton.$destroy
+    vm.$el.remove()
+    vm.$destroy
 }
-//gButton loading
+//vm loading
 {
     const Constructor = Vue.extend(Button)
-    const gButton=new Constructor({
+    const vm=new Constructor({
         propsData: {
             icon:'setting',
             loading:true
         }
     })
 
-    gButton.$mount()
-    let useElement=gButton.$el.querySelector('use')
+    vm.$mount()
+    let useElement=vm.$el.querySelector('use')
     let href=useElement.getAttribute('xlink:href')
     expect(href).to.eq('#icon-loading')
-    gButton.$el.remove()
-    gButton.$destroy
+    vm.$el.remove()
+    vm.$destroy
 }
-//gButton icon是左还是右
+//vm icon是左还是右
 {
     const div=document.createElement('div')
     document.body.appendChild(div)
     const Constructor = Vue.extend(Button)
-    const gButton=new Constructor({
+    const vm=new Constructor({
         propsData: {
             icon:'setting',
             iconPosition:'right'
         }
     })
 
-    gButton.$mount(div)
-    let svg=gButton.$el.querySelector('svg')
+    vm.$mount(div)
+    let svg=vm.$el.querySelector('svg')
     let {order}=window.getComputedStyle(svg)
     expect(order).to.eq("2")
-    gButton.$el.remove()
-    gButton.$destroy
+    vm.$el.remove()
+    vm.$destroy
 }
 {
     const div=document.createElement('div')
     document.body.appendChild(div)
-    const Constructor=Vue.extend(Button)
-    const gButton=new Constructor({
-        propsData:{
-            icon:'setting'
+    const Constructor = Vue.extend(Button)
+    const vm=new Constructor({
+        propsData: {
+            icon:'setting',
+            iconPosition:'left'
         }
     })
-    gButton.$mount(div)
-    gButton.$on('click',function () {
-        expect(1).to.eq(2)
-        console.log(111);
-    })
-    let button=gButton.$el
 
+    vm.$mount(div)
+    let svg=vm.$el.querySelector('svg')
+    let {order}=window.getComputedStyle(svg)
+    expect(order).to.eq("1")
+    vm.$el.remove()
+    vm.$destroy
+}
+
+//mock
+//chai spies
+{
+
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'setting'
+        }
+    })
+    vm.$mount()
+    let spy = chai.spy(function(){})
+
+    vm.$on('click', spy)
+    // 希望这个函数被执行
+    let button = vm.$el
     button.click()
-    console.log(button);
-    // gButton.$el.remove()
-    // gButton.$destroy
+    expect(spy).to.have.been.called()
 }
