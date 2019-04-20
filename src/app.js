d4719080779a2e5431e25910e95eb66dea6d398d
zhuanghaixin <649308293@qq.com>
2019-04-20 13:07:02 +0800

    [vue造轮子]:完成三个测试用例，setting,loading.right

--- src/app.js
+++ src/app.js
@@ -13,3 +13,83 @@ new Vue({
         loading3:true
     }
 })
+
+
+
+import chai from 'chai'
+const expect=chai.expect
+//单元测试
+//gButton setting
+{
+    const Constructor = Vue.extend(Button)
+    const gButton=new Constructor({
+        propsData: {
+            icon:'setting'
+        }
+    })
+
+    gButton.$mount()
+    let useElement=gButton.$el.querySelector('use')
+    let href=useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#icon-setting')
+    //清除button,防止内存占用
+    gButton.$el.remove()
+    gButton.$destroy
+}
+//gButton loading
+{
+    const Constructor = Vue.extend(Button)
+    const gButton=new Constructor({
+        propsData: {
+            icon:'setting',
+            loading:true
+        }
+    })
+
+    gButton.$mount()
+    let useElement=gButton.$el.querySelector('use')
+    let href=useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#icon-loading')
+    gButton.$el.remove()
+    gButton.$destroy
+}
+//gButton icon是左还是右
+{
+    const div=document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const gButton=new Constructor({
+        propsData: {
+            icon:'setting',
+            iconPosition:'right'
+        }
+    })
+
+    gButton.$mount(div)
+    let svg=gButton.$el.querySelector('svg')
+    let {order}=window.getComputedStyle(svg)
+    expect(order).to.eq("2")
+    gButton.$el.remove()
+    gButton.$destroy
+}
+{
+    const div=document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor=Vue.extend(Button)
+    const gButton=new Constructor({
+        propsData:{
+            icon:'setting'
+        }
+    })
+    gButton.$mount(div)
+    gButton.$on('click',function () {
+        expect(1).to.eq(2)
+        console.log(111);
+    })
+    let button=gButton.$el
+
+    button.click()
+    console.log(button);
+    // gButton.$el.remove()
+    // gButton.$destroy
+}
