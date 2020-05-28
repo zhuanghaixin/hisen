041fc33a9a94569a236be2cc50392df1062403df
zhuanghaixin <649308293@qq.com>
2020-05-28 10:54:03 +0800

    test(col.test.js): 完成col.test.js的测试

--- test/col.test.js
+++ test/col.test.js
@@ -4,6 +4,7 @@ const expect = chai.expect;
 import Vue from 'vue'
 import Col from '../src/col'
 
+
 Vue.config.productionTip = false
 Vue.config.devtools = false
 
@@ -14,30 +15,49 @@ describe('Col', () => {
     it('接收 span 属性', () => {
         const div=document.createElement('div')
         document.body.append(div)
-        const Constructor = Vue.extend(Row)
+        const Constructor = Vue.extend(Col)
         const vm = new Constructor({
             propsData: {
                 span:1
             }
         }).$mount(div)
         const element=vm.$el
-        // console.log(element);
-        expect(vm.$el.classList.contains('col-1')).to.eq(false)
+        console.log(111111111111111)
+        console.log(element);
+        expect(vm.$el.classList.contains('col-1')).to.eq(true)
         div.remove()
         vm.$destroy()
     })
-    it('接收 span 属性', () => {
+    it('接收 offset', () => {
         const div=document.createElement('div')
         document.body.append(div)
-        const Constructor = Vue.extend(Row)
+        const Constructor = Vue.extend(Col)
         const vm = new Constructor({
             propsData: {
                 offset:1
             }
         }).$mount(div)
         const element=vm.$el
-        // console.log(element);
-        expect(vm.$el.classList.contains('offset-1')).to.eq(false)
+        console.log(2222222222)
+        console.log(element);
+        expect(vm.$el.classList.contains('offset-1')).to.eq(true)
+        div.remove()
+        vm.$destroy()
+    })
+    it('接收 pc 属性', () => {
+        const div=document.createElement('div')
+        document.body.append(div)
+        const Constructor = Vue.extend(Col)
+        const vm = new Constructor({
+            propsData: {
+               pc:{span:1,offset:2}
+            }
+        }).$mount(div)
+        const element=vm.$el
+        console.log(33333333)
+        console.log(element);
+        expect(vm.$el.classList.contains('col-pc-1')).to.eq(true)
+        expect(vm.$el.classList.contains('offset-pc-2')).to.eq(true)
         div.remove()
         vm.$destroy()
     })
